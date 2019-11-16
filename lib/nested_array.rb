6396d18d6291a41683ff247bbc359b070fe9d64f
kowalix007 <kowalix007@gmail.com>
2019-11-16 02:05:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -55,8 +55,8 @@ def matrix_update(matrix, row, column, new_value)
   # Return the updated matrix
 
       local_copy_of_matrix = assembled_matrix.dup
-      local_copy_of_matrix[0][2]=["Tiny Ewell"]
-
+      local_copy_of_matrix[0][2]= "Tiny Ewell"
+result = local_copy_of_matrix
 
 end
 
